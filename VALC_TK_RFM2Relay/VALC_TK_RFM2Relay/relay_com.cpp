f6032b63741f64d1a887bc2c53445636ded01e9c
infrapale <infrapale@gmail.com>
2020-10-11 10:59:09 +0300

    TK terminal working OK

---
+++ VALC_TK_RFM2Relay/VALC_TK_RFM2Relay/relay_com.cpp
@@ -0,0 +1,72 @@
+
+#include "relay_com.h"
+
+SoftwareSerial softCom(SOFT_RX_PIN, SOFT_TX_PIN); 
+
+byte softComState;
+char softComBuff[SCOM_BUFF_LEN];
+short int softComBuffIndx =0;
+short int i;
+boolean softComFlag;
+
+boolean SoftComFlag(void){
+  return softComFlag;
+}
+
+void InitSoftCom (void){
+  pinMode(SOFT_RX_PIN, INPUT_PULLUP);
+  pinMode(SOFT_TX_PIN, OUTPUT);
+  softCom.begin(9600);
+  //softCom.listen();
+  
+}
+
+void SendSoftcomRelayMsg(char unit,char relay,char func){
+    char soft_buff[16]= "";
+    uint8_t i= 0;
+    soft_buff[i++] = '<';
+    soft_buff[i++] = 'I';
+    soft_buff[i++] = 'O';
+    //soft_buff[i++] = '#';
+    //soft_buff[i++] = 'R';
+    soft_buff[i++] = unit;
+    soft_buff[i++] = 'R';
+    soft_buff[i++] = relay;
+    switch(func){
+        case '0':
+            soft_buff[i++] = 'C';
+            break;  
+        case '1':
+            soft_buff[i++] = 'S';
+            break;  
+        case 'T':
+            soft_buff[i++] = 'S';
+            break;  
+    }
+    // soft_buff[i++] = '=';
+    // soft_buff[i++] = func;
+    soft_buff[i++] = '>';
+    soft_buff[i++] = '\r';
+    // softCom.println(soft_buff);
+    // Serial.print("SendSoftcomRelayMsg: ");
+    Serial.println(soft_buff);
+
+
+     
+}
+void SendSoftCom(const char *b){
+  Serial.println(b);
+  softCom.print('<');
+  softCom.print(b);
+  softCom.println(">");
+ 
+}
+
+ void DebugSoftCom(void){
+ 
+ if (softCom.available()>0){
+     char c;
+     c = softCom.read();
+     Serial.print(c);
+  }
+ }
